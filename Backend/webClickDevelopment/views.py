9d4469a30fc34f850316f4e190cea4e6f5846e22
Juan Andrés Candia Jara <jandres.candia18@gmail.com>
2024-06-04 11:12:29 -0400

    SI

--- Backend/webClickDevelopment/views.py
+++ Backend/webClickDevelopment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def crud(request):
+    usuarios = Usuario.objects.all()
+    context = {
+        'usuarios': usuarios,
+    }
+    return render(request, 'crud.html', context)
