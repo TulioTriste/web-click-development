516a87f693d4d66ed487953d57cb4a292e4bebb4
CETECOM <33809410+TulioTriste@users.noreply.github.com>
2024-06-11 11:24:18 -0400

    a

--- Backend/webClickDevelopment/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def crud(request):
-    usuarios = Usuario.objects.all()
-    context = {
-        'usuarios': usuarios,
-    }
-    return render(request, 'crud.html', context)
