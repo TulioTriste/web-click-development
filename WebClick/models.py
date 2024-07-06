7571758dd9e1cbb9de2967fd0aa67bd55aa46e2d
Juan Andrés Candia Jara <jandres.candia18@gmail.com>
2024-07-05 22:07:41 -0400

    a

--- WebClick/models.py
+++ WebClick/models.py
@@ -68,3 +68,12 @@ class Compra(models.Model):
             + " "
             + str(self.total)
         )
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    # Añade más campos según sea necesario
+
+    def __str__(self):
+        return self.nombre
